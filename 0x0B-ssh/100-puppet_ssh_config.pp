a54d9f65d758a7b00febfd471eae47faca8009f3
minasamirr <1701538@eng.asu.edu.eg>
2024-03-25 21:41:19 +0200

    Task

---
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -0,0 +1,13 @@
+#make changes to config file using Puppet
+
+include stdlib
+
+file_line { 'Turn off passwd auth':
+  path  => '/etc/ssh/ssh_config',
+  line  => 'PasswordAuthentication no',
+}
+
+file_line { 'Declare identity file':
+  path  => '/etc/ssh/ssh_config',
+  line  => 'IdentityFile ~/.ssh/school',
+}
