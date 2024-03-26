9f16c821ec335c801fddd946495d2ec484973504
minasamirr <mina430q@gmail.com>
2024-03-26 11:00:31 -0700

    task

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,5 +1,11 @@
-# Install flask version 2.1.0 using pip3
+# Install Flask version 2.1.0 using pip3
 package { 'flask':
   ensure   => '2.1.0',
+  provider => 'pip3',
+}
+
+# Install the required dependencies
+package { ['werkzeug']:
+  ensure   => 'latest',
   provider => 'pip3',
 }
